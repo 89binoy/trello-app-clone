260530cb3c66f2a8c5fcb302812bb9a786a69c1b
Binoy Saha <binoy.saha01@northsouth.edu>
2022-05-23 02:25:28 +0600

    board context done

---
+++ src/contexts/board.jsx
@@ -0,0 +1,15 @@
+import { createContext, useReducer } from 'react';
+import { borderReducer } from '../contexts/board';
+export const BoardContext = createContext();
+
+ const BoardProvider= ({children})=>{
+
+    const [boards, dispatchBoardAction] = useReducer(borderReducer, [])
+    return(
+        <BoardContext.Provider value= {{boards, dispatchBoardAction}}>
+            {children}
+        </BoardContext.Provider>
+    )
+}
+
+export default BoardProvider;
