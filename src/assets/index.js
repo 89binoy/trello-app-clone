03df71800842252cffb08f88c6ee80b4ef8876f8
Binoy Saha <binoy.saha01@northsouth.edu>
2022-06-04 00:57:07 +0600

    assets propertis added

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+import backArrowIcon from './icons/backarrow.png';
+import crossIcon from './icons/cross.png';
+import editIcon from './icons/dots.png';
+import plusIcon from './icons/plus.png';
+
+
+const icons = {
+    plusIcon,
+    crossIcon, 
+    editIcon,
+    backArrowIcon
+}
+
+export { icons };
